ea378aeb9babbae49b45b6e3f66f3b140901cc8e
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-09-17 12:56:36 -0500

    [FEAT] message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  constructor(private messageService: MessageService) {}
 
-  constructor() { }
-
-  getHeroes(): Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('Hero Service: fetched heroes');
     return heroes;
   }
 }
